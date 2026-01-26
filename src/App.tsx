1329bf2f72165efad9a5e6f0cdb78269a8688a6a
Havash <ebadian1992@gmail.com>
2026-01-26 13:35:22 +0100

    set up
    the set up for our project is done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to our Cinema Hub</h1>
+      <p>Your platform for cinema</p>
+    </div>
+  );
+}
+
+export default App;
