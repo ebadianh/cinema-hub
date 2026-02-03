4bcb00a0b6c5a7e34f4148776c396308a00685e6
LukWen-Ill <lukaswennstrom@hotmail.com>
2026-02-03 10:05:04 +0100

    added pages

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -2,6 +2,23 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import type FilmData from "../interfaces/FilmData";
 
+/*
+
+Todo:
+- fix fild data import 
+- build featured film 
+- build visas nu 
+- build filter 
+- build film card 
+- Components 
+  - buttons
+  - cards
+  - nav links
+  - modals
+  - icons
+  - 
+ 
+*/
 
 export default function StartPage() {
   return <>
