1b0336eb2af2b69ce7faca438a2ebc75470f87b1
mezea11 <christiancastellanosmeza@gmail.com>
2026-02-06 11:20:22 +0100

    Setup page routing
    - About page
    - Sign in page
    - Film details page
    - All pages now have their own routing
    - Created layout page that renders navbar and footer on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import Main from './pages/Main';
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 
 export default function App() {
 
-  return <>
-    <Main />
-  </>;
+  return useRoutes(routes);
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,10 @@
 import Cards from "../components/Cards";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer";
 
 
 export default function Main() {
   return <>  
-    <Navbar/>
     <Hero/>
     <Cards/>
-    <Footer/>
     </>;
 }
