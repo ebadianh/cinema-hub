019b130b4c3b326c86b15d3d9ebb9e282ade4e90
mezea11 <christiancastellanosmeza@gmail.com>
2026-02-06 14:11:57 +0100

    Homepage and respective components updated styling
    - Navbar has dark theme with custom hamburger
    - Hero section, new image, new styling added
    - Global css implemented, color variables, and global styling for comps
    - Layout now automatically has dark theme enabled

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,12 +3,16 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+// pages/Layout.tsx
 export default function Layout() {
   return (
-    <>
+    <div className="min-vh-100 d-flex flex-column">       {/* added min viewport for all pages */}
+
       <Navbar />
-      <Outlet />
+      <main className="flex-grow-1">       {/* Added flex for body */}
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
