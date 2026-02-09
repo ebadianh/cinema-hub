7026b2db00ffe116f18e3418b8a5ef3d067e5024
Havash <ebadian1992@gmail.com>
2026-02-09 11:37:18 +0100

    added posters
    implemented a img src in the component cards so our cards gets movie
    posters

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -102,6 +102,8 @@ export default function Cards() {
         {films.map((f) => (
           <div key={f.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
             <div className="card h-100 shadow-sm">
+              <img src={`/public/${f.id}.jpg`}
+                className="card-img-top" alt={f.title} />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title mb-1">{f.title}</h5>
 
