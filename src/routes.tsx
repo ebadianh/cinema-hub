6bd70b231f595404ae68bec57dbba469873c214d
LukWen-Ill <lukaswennstrom@hotmail.com>
2026-02-09 14:12:48 +0100

    ai-chat integrated

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,17 +5,19 @@ import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
 import FilmDetails from "./pages/FilmDetails";
 import About from "./pages/About";
+import AiChatPage from "./pages/AiChatPage";
 
 const routes = [
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "", element: <Main /> },            // "/"
-      { path: "signin", element: <SignIn /> },     // "/signin"
-      { path: "register", element: <Register /> },
-      { path: "films/:id", element: <FilmDetails /> }, // "/films/:id"
-      { path: "about", element: <About /> },            // "/"
+      { path: "/", element: <Main /> },            // "/"
+      { path: "/signin", element: <SignIn /> },     // "/signin"
+      { path: "/register", element: <Register /> },
+      { path: "/films/:id", element: <FilmDetails /> }, // "/films/:id"
+      { path: "/about", element: <About /> },            // "/"
+      { path: '/chat', element: <AiChatPage/> },
       { path: "*", element: <h1>Page not found</h1> },
     ],
   },
